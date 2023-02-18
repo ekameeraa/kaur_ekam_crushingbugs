fda44b396ffc4d017ecfcbaada47ec5c7393f776
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-02-18 15:34:13 -0500

    got in trouble
    I lost my folder mistakenly and now pasting it all

--- script/main.js
+++ script/main.js
@@ -1,9 +1,50 @@
-
+// let theButton = document.queryselector ("#buttonOne")
+// step1
 let theButtons = document.querySelectorAll("#buttonHolder img"),
     theHeading = document.querySelector("#headLine h1"),
     puzzleBoard = document.querySelector(".puzzle-board"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+    dropZones = document.querySelectorAll('.drop-zone'),
+    mainBoard = document.querySelector('.puzzle-pieces'),
+// store the dragged piece in a global variable
+	// because we need it in the handleDrop function
+    draggedPiece = null; 
 
+//step3 
+//bug2
+function changeBGImage() {
+    dropZones.forEach(zone => {
+        while (zone.firstChild) {
+            zone.removeChild(zone.firstChild);
+        }
+    });
+
+    puzzlePieces.forEach(piece => {
+        piece.classList.remove("dropped");
+        mainBoard.appendChild(piece);
+    });
+
+    puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+}
+
+function handleStartDrag() { 
+    console.log('started dragging this piece:', this);
+    draggedPiece = this;
+}
+
+function handleDragOver(e) { 
+    e.preventDefault(); 
+    console.log('dragged over me');
+}
+
+function handleDrop(e) {
+    e.preventDefault();
+    console.log('dropped something on me');
+
+// bug1
+    const dropZone = this;
+
+// check if the drop zone has a piece already
     const existingPiece = dropZone.querySelector('img');
     if (existingPiece) {
         puzzleBoard.insertBefore(draggedPiece, existingPiece);
@@ -21,7 +62,7 @@ let theButtons = document.querySelectorAll("#buttonHolder img"),
     draggedPiece = null;
 }
 
-
+// step2
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
